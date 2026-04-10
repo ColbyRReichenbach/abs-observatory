48a9d1c22536edd1709a45de2730079f2f2fd573
Colby Reichenbach <colbyrreichenbach@gmail.com>
2026-04-10 16:54:04 -0400

    Add AI surface refactor plan and terminology skeleton

---
+++ src/lib/server/ai/terminology/index.ts
@@ -0,0 +1,11 @@
+export { compileTerminologyAppendix } from "./compiler";
+export { loadTerminologySeedBundle } from "./loader";
+export { selectTerminologyBundle } from "./selector";
+export type {
+  SelectedTerminologyBundle,
+  StylePack,
+  SurfaceRule,
+  TerminologyCard,
+  TerminologySeedBundle,
+  TerminologySelectionInput,
+} from "./types";
